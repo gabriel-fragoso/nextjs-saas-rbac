9f1077f8ecff4a0dc9839d62a11c03f1c1c59d41
Gabriel <fragosooliveira773@gmail.com>
2024-08-19 11:27:41 -0300

    feat: Update permissions for ADMIN role in auth package

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -2,20 +2,32 @@ import { AbilityBuilder } from '@casl/ability'
 
 import { AppAbility } from '.'
 import { User } from './models/user'
-
-type Role = 'ADMIN' | 'MEMBER'
+import { Role } from './roles'
 
 type PermissionsByRole = (
   user: User,
-  builder: AbilityBuilder<AppAbility>
+  builder: AbilityBuilder<AppAbility>,
 ) => void
 
 export const permissions: Record<Role, PermissionsByRole> = {
-  ADMIN(_, { can }) {
+  ADMIN(user, { can, cannot }) {
     can('manage', 'all')
+
+    cannot(['transfer_ownership', 'update'], 'Organization')
+    can(['transfer_ownership', 'update'], 'Organization', {
+      ownerId: {
+        $eq: user.id,
+      },
+    })
   },
-  MEMBER(_, { can }) {
-    can('invite', 'User')
-    can('manage', 'Project')
+
+  MEMBER(user, { can }) {
+    can('get', 'User')
+    can(['create', 'get'], 'Project')
+    can(['update', 'delete'], 'Project', { ownerId: { $eq: user.id } })
+  },
+
+  BILLING(_, { can }) {
+    can('manage', 'Billing')
   },
 }
